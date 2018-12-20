b109850e096e6948595da1b3a162cffbe944ffde
Zhang Yuang <1328410180@qq.com>
2018-12-21 01:47:07 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
+mkdir -p game/images/bg
+
 curl -u zyayoung:public -o 'game/images/bg/bg sjtu eastgate.png' ftp://public.sjtu.edu.cn/bg%20sjtu%20eastgate.png
 curl -u zyayoung:public -o 'game/gui/main_menu.png' ftp://public.sjtu.edu.cn/main_menu.png
 curl -u zyayoung:public -o 'game/SourceHanSansSC-Regular.otf' ftp://public.sjtu.edu.cn/SourceHanSansSC-Regular.otf
